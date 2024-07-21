47fc3d2ae90e186898216b7bd55cc31c5b715024
hanouw <jehasite@gmail.com>
2024-07-21 17:41:11 +0900

    init commit

---
+++ src/main/java/calorify/calorify/CalorifyApplication.java
@@ -0,0 +1,13 @@
+package calorify.calorify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/calorify/calorify/CalorifyApplicationTests.java
@@ -0,0 +1,13 @@
+package calorify.calorify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
