cea955c2b59529ca877e06b6ad1d315c80d9e41e
hanouw <jehasite@gmail.com>
2024-07-22 21:36:01 +0900

    thymeleaf

--- src/main/java/calorify/calorify/CalorifyApplication.java
+++ src/main/java/calorify/calorify/CalorifyApplication.java
@@ -2,8 +2,10 @@ package calorify.calorify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+//		(exclude = SecurityAutoConfiguration.class)
 public class CalorifyApplication {
 
 	public static void main(String[] args) {
